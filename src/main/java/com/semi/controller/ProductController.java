4c78da19fb92f89f51c874fae0b7fc59d61cf98a
woorisx <suhwooree@gmail.com>
2026-04-26 11:54:21 +0900

    [우리] index 완료

--- src/main/java/com/semi/controller/ProductController.java
+++ src/main/java/com/semi/controller/ProductController.java
@@ -4,13 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
->>>>>>> 1ade278 (fix: Thymeleaf 3.1 security error on index.html)
 
 import com.semi.domain.product.Product;
 import com.semi.domain.product.ProductService;
@@ -19,31 +14,16 @@ import lombok.RequiredArgsConstructor;
 
 
 @Controller
-<<<<<<< HEAD
-@RequestMapping("/product/**")
-=======
-@RequestMapping("/product")
->>>>>>> 1ade278 (fix: Thymeleaf 3.1 security error on index.html)
+@RequestMapping({"/product", "/"})
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/list")
-<<<<<<< HEAD
-    public String getProductList(Model model) {
-        List<Product> productList = productService.getProductList();
-        // productList.toString();
-        model.addAttribute("products", productList);
-        return "index";
-    }
-    
-
-=======
+    @GetMapping({"/list", "/"})
     public String getAllProduct(Model model) {
         List<Product> products = productService.getAllProduct();
         model.addAttribute("products", products);
         return "index";
     }
->>>>>>> 1ade278 (fix: Thymeleaf 3.1 security error on index.html)
 }
--- src/main/java/com/semi/domain/product/ProductService.java
+++ src/main/java/com/semi/domain/product/ProductService.java
@@ -1,14 +1,5 @@
 package com.semi.domain.product;
 
-<<<<<<< HEAD
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import java.util.List;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Slf4j
-=======
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -16,24 +7,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
->>>>>>> 1ade278 (fix: Thymeleaf 3.1 security error on index.html)
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-<<<<<<< HEAD
-    /**
-     * 상품 전부 가져오기
-     * 
-     */
-    @Transactional
-    public List<Product> getProductList() {
-=======
     @Transactional
     public List<Product> getAllProduct(){
->>>>>>> 1ade278 (fix: Thymeleaf 3.1 security error on index.html)
         return productRepository.findAll();
     }
 }
